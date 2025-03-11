f076c628493f7b8b8b87c1ca0d04991dc35a80c7
Josh <jsbusiness305@gmail.com>
2025-03-11 17:55:07 -0400

    updated Wave component, added a changing span with dev roles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,9 +3,9 @@ export default function Hero() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center animate-slide-in-right transiiton duration-400 ease-in">
       {/* onMouseLeave={() => setBgColor(null)} */}
-      <div className="rounded-full w-[30rem] h-[30rem] flex flex-col items-center justify-center p-2 border-4 border-amber-500">
-        <h1 className="text-6xl font-bold ">Hello, I&apos;m Josh</h1>
-        <p className="text-2xl font-bold">Full Stack Developer</p>
+      <div className="rounded-full sm:w-[20rem] sm:h-[20rem] md:w-[30rem] md:h-[30rem] flex flex-col items-center justify-center p-2 border-4 border-amber-500">
+        <h1 className=" sm:text-xl md:text-6xl font-bold ">Hello, I&apos;m Josh</h1>
+        <p className="sm:text-lg md:text-2xl font-bold">Full Stack Developer</p>
       </div>
     </div>
   );
