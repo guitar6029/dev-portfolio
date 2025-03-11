9dcd9e99fbc7e4df99a276dbce41b2ff64ff9671
Josh <jsbusiness305@gmail.com>
2025-03-11 16:53:51 -0400

    updated styles for Header, and Hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header>
-            <div className="flex flex-row items-center  bg-neutral-900 border-b-2 border-neutral-500">
+            <div className="flex flex-row items-center  bg-neutral-900 border-b-1 border-neutral-500">
                 <Link href="/" className="sm:text-xl md:text-2xl p-3 hover:bg-amber-300 hover:text-black transition duration-300 ease-in">Home</Link>
                 <Link href="/projects" className="sm:text-xl md:text-2xl p-3 hover:bg-amber-300 hover:text-black transition duration-300 ease-in">Projects</Link>
                 <Link href="/about" className="sm:text-xl md:text-2xl p-3 hover:bg-amber-300 hover:text-black transition duration-300 ease-in">About</Link>
