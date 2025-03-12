667218b99d14ad40d7f1865cabfcbc88a1dc4cd1
Josh <jsbusiness305@gmail.com>
2025-03-12 15:20:56 -0400

    added not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-4xl font-bold text-amber-400">Page Not Found</h1>
+      <Link
+        href={"/"}
+        className="text-2xl p-3 md:w-[15rem] md:text-center rounded-xl bg-amber-50 text-black hover:bg-amber-500 transition duration-300 ease-in"
+      >
+        Go to Home
+      </Link>
+      <Link
+        href={"/projects"}
+        className="text-2xl p-3 md:w-[15rem] md:text-center rounded-xl bg-amber-50 text-black hover:bg-amber-500 transition duration-300 ease-in"
+      >
+        Go to Projects
+      </Link>
+    </div>
+  );
+}
