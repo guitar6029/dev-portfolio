b3f0fe20109a670fcd82fb14a3690121d824f591
Josh <jsbusiness305@gmail.com>
2025-03-11 22:33:11 -0400

    added lucide icons, added styles to the card projects, updated Header

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function Project(){
+import { connectToDatabase } from "@/lib/mongodb"
+import { ObjectId } from "mongodb"
+import Link from "next/link";
+
+export default async function Project({params: {id}} : {params: {id: string}}) {
+    
+const { db } = await connectToDatabase();
+
+const project = await db
+.collection("github-projects")
+.findOne({ _id: new ObjectId(id) });
+    
     return (
-        <div className="min-h-screen p-4">
+        <div className="min-h-screen p-4 flex flex-col">
+            <div className="flex flex-col p-4">
+
+            </div>
             <h1 className="text-4xl font-bold text-amber-400 underline">Project</h1>
+            <h1 className="text-2xl font-bold">{project?.title}</h1>
+            
         </div>
     )
 }
---
+++ src/lib/mongodb.ts
@@ -0,0 +1,47 @@
+import { MongoClient, Db } from "mongodb";
+
+// Ensure the MongoDB URI is provided
+if (!process.env.MONGO_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable in your .env file"
+  );
+}
+
+// this is the new connection URI for the mongodb stored in the env variable
+const client = new MongoClient(process.env.MONGO_URI as string);
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+// this will connect to db
+export async function connectToDatabase() {
+  //if already cached, return the cached db
+  if (cachedClient && cachedDb) {
+    console.log("Using cached database connection");
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  //if not cached, connect to db
+  await client.connect();
+
+  // get the db
+  const db = client.db("portfolio");
+
+  console.log("Successfully connected to database:", db.databaseName);
+
+  // Check if the collection exists
+  const collections = await db.listCollections().toArray();
+  const collectionNames = collections.map((col) => col.name);
+
+  if (!collectionNames.includes("github-projects")) {
+    console.warn("Collection 'github-projects' not found in database.");
+  } else {
+    console.log("Collection 'github-projects' found.");
+  }
+
+  //cache the client and db
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
