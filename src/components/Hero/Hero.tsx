f03fb03f8d821eb8a37072ec5b3d79f51b3881fa
Josh <jsbusiness305@gmail.com>
2025-03-11 23:42:28 -0400

    update Hero component (font size)

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,8 +2,8 @@ export default function Hero() {
   return (
     <div className="h-fit mt-5 flex flex-col items-center justify-center animate-slide-in-right transiiton duration-400 ease-in">
       {/* onMouseLeave={() => setBgColor(null)} */}
-      <div className="rounded-full sm:w-[20rem] sm:h-[20rem] md:w-[30rem] md:h-[30rem] flex flex-col items-center justify-center p-2 border-4 border-amber-500">
-        <h1 className=" sm:text-xl md:text-6xl font-bold ">
+      <div className="rounded-full w-[20rem] h-[20rem] md:w-[30rem] md:h-[30rem] flex flex-col items-center justify-center p-2 border-4 border-amber-500">
+        <h1 className="text-2xl md:text-6xl font-bold ">
           Hello, I&apos;m Josh
         </h1>
       </div>
