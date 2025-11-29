bc89a37cd11660e6ef1ac8e789d351c5de6899fe
jsdev305 <jsdev305@gmail.com>
2025-11-29 11:42:55 -0500

    added skill custom svg shape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
         <AnimatedShapesMoving
           shapes={5}
           direction="left"
-          className="w-4 h-4 absolute top-10 right-20 rounded-xl border-2 animated-shapes"
+          className="w-4 h-4 absolute top-10 right-20 rounded-xl border-4 animated-shapes z-1"
         />
       </div>
 
@@ -187,7 +187,7 @@ export default function Home() {
         <AnimatedShapesMoving
           shapes={5}
           direction="right"
-          className="absolute top-50 animated-shapes-up-down w-6 h-6 border-2"
+          className="absolute top-50 animated-shapes-up-down w-6 h-6 border-2 z-1 opacity-25"
         />
 
         <div className="z-10 flex flex-col items-center justify-center gap-6">
@@ -209,25 +209,42 @@ export default function Home() {
           </div>
           {TECH.map((techItem: SkillTech, index: number) => {
             return (
-              <div key={index} className="flex flex-col items-center gap-6">
+              <div key={index} className="flex flex-col items-center gap-10">
                 <span className="text-4xl text-(--blue)">{techItem.title}</span>
                 <div className="flex flex-row flex-wrap items-center justify-center gap-4">
                   {techItem.stack.map((item: string, idx: number) => {
                     return (
                       <div
+                        className="relative w-50 h-50 flex items-center justify-center animate-slide-in-left"
                         key={idx}
-                        className="w-50 cursor-default trns hover:bg-(--bg-hover) hover:text-(--blue-soft) h-35 border-2 rounded-xl flex flex-col items-center justify-center gap-2 animate-slide-in-left"
-                        style={
-                          {
-                            "--delay": `${(idx + 1) * 340}ms`,
-                          } as React.CSSProperties
-                        }
                       >
-                        <TechIcon
-                          name={item as TechName}
-                          className="text-4xl"
-                        />
-                        <span className="capitalize text-2xl">{item}</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="absolute inset-0 w-full h-full"
+                          viewBox="0 0 161 133"
+                          fill="none"
+                        >
+                          <path
+                            d="M1.76201 55.7323L29.6411 5.63712C31.4058 2.46608 34.75 0.5 38.3791 0.5H150C155.523 0.5 160 4.97715 160 10.5V55.9304C160 57.2957 159.72 58.6465 159.179 59.8996L130.608 125.969C129.025 129.63 125.418 132 121.429 132H10.5C4.97715 132 0.5 127.523 0.5 122V60.5952C0.5 58.8933 0.93438 57.2195 1.76201 55.7323Z"
+                            stroke="gray"
+                            strokeWidth={2}
+                          />
+                        </svg>
+
+                        <div
+                          className="relative z-10 cursor-default trns flex flex-col items-center justify-center gap-2"
+                          style={
+                            {
+                              "--delay": `${(idx + 1) * 340}ms`,
+                            } as React.CSSProperties
+                          }
+                        >
+                          <TechIcon
+                            name={item as TechName}
+                            className="text-4xl"
+                          />
+                          <span className="capitalize text-2xl">{item}</span>
+                        </div>
                       </div>
                     );
                   })}
